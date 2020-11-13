6f06f664d6aae0ca426256059be33eab5c68e140
Ard2238 <abhishekrdas28@gmail.com>
2020-11-14 02:13:49 +0530

    Refactored Code to restrict letters and spl characters at the end

--- Pin.js
+++ Pin.js
@@ -8,8 +8,8 @@ class PinCode{
         return this._pin;
     }
     set pin(pin){
-        let letterRegex = RegExp('^[^a-zA-z]+')
-        let splRegex = RegExp('[!@#$^&%*()+=-[]\/{}|:<>?,.]+')
+        let letterRegex = RegExp('^[^a-zA-z]+$')
+        let splRegex = RegExp('^[!@#$^&%*()+=-[]\/{}|:<>?,.]+$')
         let pinRegex = RegExp('^[0-9]{6}$')
         if(letterRegex.test(pin)&& !splRegex.test(pin) && pinRegex.test(pin)){
             this._pin = pin;
@@ -22,8 +22,10 @@ class PinCode{
 try{
     let pincode = new PinCode(453001)
     console.log(pincode)
-    let pincode1 = new PinCode(!400088)
+    let pincode1 = new PinCode("400088B")
     console.log(pincode1)
+    let pincode2 = new PinCode("400088@")
+    console.log(pincode2)
 }catch(e){
     console.log("Error :  " + e)
 }
